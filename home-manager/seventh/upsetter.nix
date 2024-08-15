89279ecf2c4c3987e5851bbce0d11a14260adb00
Chess7th <chess7th@pm.me>
2024-08-15 15:00:34 +0200

    feat: comment out hyprland env settings

--- home-manager/seventh/upsetter.nix
+++ home-manager/seventh/upsetter.nix
@@ -313,15 +313,15 @@
     slack
   ];
 
-  wayland.windowManager.hyprland.settings.env = [
-    # "WLR_DRM_DEVICES,/home/${config.home.username}/.config/hypr/card"
-    # "AQ_DRM_DEVICES,/home/${config.home.username}/.config/hypr/card"
-    # "AQ_DRM_DEVICES,/dev/dri/card1"
-    # "AQ_TRACE,1"
-    "HYPLAND_TRACE,1"
-    "XDG_SESSION_TYPE,wayland"
-    "MOZ_ENABLE_WAYLAND,1"
-  ];
+  # wayland.windowManager.hyprland.settings.env = [
+  #   # "WLR_DRM_DEVICES,/home/${config.home.username}/.config/hypr/card"
+  #   # "AQ_DRM_DEVICES,/home/${config.home.username}/.config/hypr/card"
+  #   # "AQ_DRM_DEVICES,/dev/dri/card1"
+  #   # "AQ_TRACE,1"
+  #   # "HYPLAND_TRACE,1"
+  #   # "XDG_SESSION_TYPE,wayland"
+  #   # "MOZ_ENABLE_WAYLAND,1"
+  # ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";

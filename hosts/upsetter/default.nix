26e58804a06b858c3d49a6931e49084d905dd6aa
Chess7th <chess7th@pm.me>
2024-08-15 15:00:15 +0200

    feat: remove unused nvidia stuff

--- hosts/upsetter/default.nix
+++ hosts/upsetter/default.nix
@@ -56,43 +56,6 @@
 
   programs.hyprland.enable = true;
 
-  # nvidia bullshit
-
-  # boot.kernelParams = ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
-
-  # hardware.nvidia = {
-  #   # Modesetting is required.
-  #   # Use integrated options with nvidia-offload for battery saving
-  #   modesetting.enable = true;
-  #
-  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #   powerManagement.enable = true;
-  #   # Fine-grained power management. Turns off GPU when not in use.
-  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #   powerManagement.finegrained = true;
-  #
-  #   # Enable dynamic power management.
-  #   # Dynamic Boost balances power between the CPU and the GPU for improved
-  #   # performance on supported laptops using the nvidia-powerd daemon.
-  #   dynamicBoost.enable = false;
-  #
-  #   # Use the NVidia open source kernel module (not to be confused with the
-  #   # independent third-party "nouveau" open source driver).
-  #   # Support is limited to the Turing and later architectures. Full list of
-  #   # supported GPUs is at:
-  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-  #   # Only available from driver 515.43.04+
-  #   # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-  #   open = false;
-  #
-  #   # Enable the Nvidia settings menu,
-  #   # accessible via `nvidia-settings`.
-  #   nvidiaSettings = false;
-  #
-  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
-
   # steam bullshit
   hardware.opengl = {
     enable = true;

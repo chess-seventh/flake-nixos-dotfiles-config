20e60fdd883f5404f4631995adfa6a56c35dee63
Chess Seventh <chess7th@pm.me>
2024-09-13 16:42:00 +0200

    feat: change dotfile paths for upsetter seventh

--- home-manager/seventh/upsetter.nix
+++ home-manager/seventh/upsetter.nix
@@ -39,44 +39,36 @@ config,
   home.file = {
     # ".config/hypr/card".source = config.lib.file.mkOutOfStoreSymlink "/dev/dri/by-path/pci-0000:06:00.0-card";
 
-    ".config/hypr/macchiato.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/hypr/macchiato.conf";
-    ".config/avizo/config.ini".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/avizo/config.ini";
+    ".config/hypr/macchiato.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/hypr/macchiato.conf";
+    ".config/hypr/pyprland.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/hypr/pyprland.toml";
 
-    ".config/hypr/pyprland.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/hypr/pyprland.toml";
+    ".config/avizo/config.ini".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/avizo/config.ini";
 
-    ".config/lazygit/config.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/lazygit/config.yml";
+    ".config/lazygit/config.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/lazygit/config.yml";
 
-    ".config/qutebrowser/autoconfig.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/autoconfig.yml";
-    ".config/qutebrowser/config.py".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/config.py";
-    ".config/qutebrowser/quickmarks".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/quickmarks";
+    ".config/qutebrowser/autoconfig.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/autoconfig.yml";
+    ".config/qutebrowser/config.py".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/config.py";
+    ".config/qutebrowser/quickmarks".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/quickmarks";
 
-    ".config/qutebrowser/userscripts/cast".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/cast";
-    ".config/qutebrowser/userscripts/gitclone".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/gitclone";
-    ".config/qutebrowser/userscripts/password_fill".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/password_fill";
-    ".config/qutebrowser/userscripts/qute_pass".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/qute_pass";
-    ".config/qutebrowser/userscripts/readability".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/readability";
-    ".config/qutebrowser/userscripts/readability-js".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/readability-js";
-    ".config/qutebrowser/userscripts/taskadd".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/taskadd";
-    ".config/qutebrowser/userscripts/translate".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/qutebrowser/userscripts/translate";
+    ".config/qutebrowser/userscripts/cast".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/cast";
+    ".config/qutebrowser/userscripts/gitclone".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/gitclone";
+    ".config/qutebrowser/userscripts/password_fill".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/password_fill";
+    ".config/qutebrowser/userscripts/qute_pass".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/qute_pass";
+    ".config/qutebrowser/userscripts/readability".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/readability";
+    ".config/qutebrowser/userscripts/readability-js".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/readability-js";
+    ".config/qutebrowser/userscripts/taskadd".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/taskadd";
+    ".config/qutebrowser/userscripts/translate".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/qutebrowser/userscripts/translate";
 
-    ".config/dunst/dunstrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/dunst/dunstrc";
+    ".config/dunst/dunstrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/dunst/dunstrc";
 
-    ".config/tmate/tmate_colors.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmate/tmate_colors.conf";
-    ".config/tmate/tmate_keybindings.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmate/tmate_keybindings.conf";
-    ".config/tmate/tmate_options.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmate/tmate_options.conf";
-    ".config/tmate/tmate_status_bar.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmate/tmate_status_bar.conf";
+    ".config/tmate/tmate_colors.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/tmate/tmate_colors.conf";
+    ".config/tmate/tmate_keybindings.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/tmate/tmate_keybindings.conf";
+    ".config/tmate/tmate_options.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/tmate/tmate_options.conf";
+    ".config/tmate/tmate_status_bar.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/tmate/tmate_status_bar.conf";
 
-    ".config/htop/htoprc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/htop/htoprc";
+    ".config/htop/htoprc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/htop/htoprc";
 
-    ".config/btop/btop.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/btop/btop.conf";
-
-    ".config/tmux/tmux_colors.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmux/tmux_colors.conf";
-    ".config/tmux/tmux_keybindings.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmux/tmux_keybindings.conf";
-    ".config/tmux/tmux_options.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmux/tmux_options.conf";
-    ".config/tmux/tmux_plugins_install.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmux/tmux_plugins_install.conf";
-    ".config/tmux/tmux_plugins_setting.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmux/tmux_plugins_setting.conf";
-    ".config/tmux/tmux_status_bar.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/dotfiles/config/tmux/tmux_status_bar.conf";
-
+    ".config/btop/btop.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/git.sr.ht/chess7th/flake-nixos-config/dotfiles/btop/btop.conf";
   };
 
   home.packages = with pkgs; [

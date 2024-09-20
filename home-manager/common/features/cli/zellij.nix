71d0828beaa3e6c509eb4d5ac70bf4e5b52f753f
Chess Seventh <chess7th@pm.me>
2024-09-20 11:05:08 +0200

    feat(zellij): add gruvbox theme

--- home-manager/common/features/cli/zellij.nix
+++ home-manager/common/features/cli/zellij.nix
@@ -3,6 +3,10 @@
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      theme = "gruvbox-dark";
+      # themes.custom.fg = "#ffffff";
+    };
   };
 }
 

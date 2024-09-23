3b99eb83f560dc7cb8cf2ecd77726e68031d41be
Chess Seventh <chess7th@pm.me>
2024-09-23 23:03:58 +0200

    chore(zellij): add links and remove useless line

--- home-manager/common/features/cli/zellij.nix
+++ home-manager/common/features/cli/zellij.nix
@@ -1,11 +1,11 @@
-{ ... }:
-{
+{ ... }: {
+# https://github.com/zellij-org/zellij/blob/main/zellij-utils/assets/config/default.kdl
+# https://zellij.dev/documentation/keybindings
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
     settings = {
       theme = "gruvbox-dark";
-      # themes.custom.fg = "#ffffff";
     };
   };
 }

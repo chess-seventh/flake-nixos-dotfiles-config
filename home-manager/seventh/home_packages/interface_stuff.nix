f37acccc861c9de5bda910ef47e5951dd371bde9
Chess Seventh <chess7th@pm.me>
2024-09-25 17:28:48 +0200

    feat: split packages in files

---
+++ home-manager/seventh/home_packages/interface_stuff.nix
@@ -0,0 +1,51 @@
+{
+inputs,
+lib,
+pkgs,
+config,
+...
+}: {
+  home.packages = with pkgs; [
+    avizo
+    catppuccin-cursors.macchiatoTeal
+    catppuccin-gtk
+    catppuccin-kvantum
+    colloid-icon-theme
+    dunst
+    feh
+    fuzzel
+    geeqie
+    grim
+    hyprcursor
+    hypridle
+    hyprpaper
+    hyprpicker
+    imv
+    mpv
+    numix-icon-theme-circle
+    obsidian
+    pamixer
+    pavucontrol
+    pulseaudio
+    pyprland
+    rofi
+    rofimoji
+    slurp
+    starship
+    sway
+    swaybg
+    waypaper
+    wdisplays
+    wl-clipboard
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-wlr
+    xfce.thunar
+    yubikey-manager
+    yubikey-manager-qt
+    yubikey-personalization
+    zathura
+  ];
+}
+
---
+++ home-manager/seventh/home_packages/web_tools.nix
@@ -0,0 +1,21 @@
+{
+inputs,
+lib,
+pkgs,
+config,
+...
+}: {
+  home.packages = with pkgs; [
+    brave
+    chromium
+    spotify
+    lutris
+    slack
+    firefox
+    protonmail-bridge
+    protonvpn-gui
+    qutebrowser
+  ];
+}
+
+

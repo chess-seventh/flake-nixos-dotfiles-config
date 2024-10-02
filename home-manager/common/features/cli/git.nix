026a442ca27f513edaf04bb46478e1d4f4be23ab
Chess Seventh <chess7th@pm.me>
2024-10-02 12:26:35 +0200

    feat: add gpg signing key

--- home-manager/common/features/cli/git.nix
+++ home-manager/common/features/cli/git.nix
@@ -6,6 +6,10 @@
 
     userEmail = "chess7th@pm.me";
     userName = "Chess Seventh";
+    signing = {
+      key = "DBC4A6C7DF1A29A18558D2B3A7D05D40C1C0C0F4";
+      signByDefault = true;
+    };
     delta = {
       enable = true;
       options = {
@@ -20,50 +24,50 @@
     };
 
     aliases = {
-      # current working tree status using the short format
+# current working tree status using the short format
       s  = "status";
       st = "status --short";
 
       brd = "!git branch | fzf -m --print0 | tr -d ' ' | xargs -0 git branch -D";
 
-      # # delete merged branches
+# # delete merged branches
       dm = "!git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d";
 
-      # ctags     = "!.git/hooks/ctags";
+# ctags     = "!.git/hooks/ctags";
 
-      # # PULLS
-      # # nice git pull
+# # PULLS
+# # nice git pull
       up        = "!git remote update -p; git merge --ff-only @{u}";
       ploh      = "!git pull origin `git rev-parse --abbrev-ref HEAD` --ff-only";
 
-      # # COMMITS
+# # COMMITS
       com       = "commit";
-      ca        = "commit -a --amend";
-      wip       = "commit -a --message=WIP";
-      cm        = "commit -m";
-      cam       = "commit -am";
-      cane      = "commit --amend --no-edit";
+      ca        = "commit -a -S --amend";
+      wip       = "commit -a -S --message=WIP";
+      cm        = "commit -m -S";
+      cam       = "commit -am -S";
+      cane      = "commit --amend --no-edit -S";
 
-      # # Retrieves last modified files
-      # fixup      = !"git status --short | grep '^.[MARCD]' |  sed 's/^.. //' | fzf | xargs -o git add $@  && git log -n 20 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup && git rebase -i --autosquash origin/master";
+# # Retrieves last modified files
+# fixup      = !"git status --short | grep '^.[MARCD]' |  sed 's/^.. //' | fzf | xargs -o git add $@  && git log -n 20 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup && git rebase -i --autosquash origin/master";
       ap         = "add -p";
-      # autosquash = "!git rebase -i --autosquash origin/master";
+# autosquash = "!git rebase -i --autosquash origin/master";
 
-      # # COMPARE DIFFS LOGS GRAPHS
+# # COMPARE DIFFS LOGS GRAPHS
       d1        = "diff HEAD~1";
       graph     = "log --graph --date=format:'%e %b %Y' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit";
       co        = "checkout";
       cob       = "checkout -b";
-      # conflicts = "!git diff --name-only | uniq | xargs nvim";
+# conflicts = "!git diff --name-only | uniq | xargs nvim";
       recent    = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
       dfr       = "!git diff origin `git rev-parse --abbrev-ref HEAD`";
 
       histline   = "log --pretty=oneline";
       histdiff   = "log --stat";
 
-      # preparemr = !"git log --pretty=format:'-%C(yellow)%d%Creset %s -- %C(cyan)(%ci)%Creset %C(bold blue)@%al%Creset' $(git rev-parse --abbrev-ref HEAD) --not $(git for-each-ref --format='%(refname)' refs/heads/ | grep -v $(git rev-parse --abbrev-ref HEAD) | grep -v Merge ) | sed 's/([^)]*) //' ";
+# preparemr = !"git log --pretty=format:'-%C(yellow)%d%Creset %s -- %C(cyan)(%ci)%Creset %C(bold blue)@%al%Creset' $(git rev-parse --abbrev-ref HEAD) --not $(git for-each-ref --format='%(refname)' refs/heads/ | grep -v $(git rev-parse --abbrev-ref HEAD) | grep -v Merge ) | sed 's/([^)]*) //' ";
 
-      # # CHECKOUT
+# # CHECKOUT
       out = "checkout -- .";
 
       chom  = "checkout master";
@@ -79,42 +83,42 @@
 
       recentbranch = "branch --sort=-committerdate";
 
-      # # show a nice log commit tree
+# # show a nice log commit tree
       l         = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -p";
       lo        = "log --pretty=oneline -n 40 --graph --abbrev-commit";
       lg        = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' -n 20 --abbrev-commit";
       weeklylog = "!git log --since='`$(date -d \"last week\" +%Y-%m-%d)`' --no-merges --pretty=format:'%C(green)%cI%Creset %C(blue)%h%Creset %C(yellow)[%cl]%Creset %n%B'";
 
 
-      # # diff between the latest commit and the current state
-      # #d = !"git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat --color=always" | less -r
+# # diff between the latest commit and the current state
+# #d = !"git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat --color=always" | less -r
       d = "diff";
 
-      # # ERRORS
+# # ERRORS
       undo      = "reset HEAD~1 --mixed";
       savepoint = "!git add -A && git commit -qm 'WIPE SAVEPOINT' && git reset HEAD~1 --hard";
 
-      # # PUSHES
+# # PUSHES
       po        = "push origin";
       poh       = "!git push origin `git rev-parse --abbrev-ref HEAD`";
       pof       = "!git push origin `git rev-parse --abbrev-ref HEAD` --force";
 
-      ## Github pushes
+## Github pushes
       gpoh       = "!git push origin-gh `git rev-parse --abbrev-ref HEAD`";
       gpof       = "!git push origin-gh `git rev-parse --abbrev-ref HEAD` --force";
 
-      # # REBASE
+# # REBASE
       rba        = "rebase --abort";
       rbc        = "rebase --continue";
 
       rom       = "rebase -i origin/master";
       roma      = "rebase -i origin/main";
 
-      ## Github rebases
+## Github rebases
       grom       = "rebase -i origin-gh/master";
       groma      = "rebase -i origin-gh/main";
 
-      # # WORK REBASES
+# # WORK REBASES
       romexp = "rebase -i origin/release/experimental";
       romdev = "rebase -i origin/release/dev";
       romstg = "rebase -i origin/release/stg";
@@ -126,34 +130,34 @@
       pom   = "pull origin master";
       poma  = "pull origin main";
 
-      ## Github pulls
+## Github pulls
       gprom  = "pull --rebase origin master";
       gproma = "pull --rebase origin main";
 
       gpom   = "pull origin master";
       gpoma  = "pull origin main";
 
-      # # WORK PULL REBASES
+# # WORK PULL REBASES
       prexp  = "pull --rebase origin release/experimental";
       prodev = "pull --rebase origin release/dev";
       prostg = "pull --rebase origin release/stg";
       proprd = "pull --rebase origin release/prd";
 
-      # # FETCH
+# # FETCH
       fo = "fetch origin";
       r1 = "reset HEAD~1";
 
-      # # RIBBON
+# # RIBBON
       ribbon  = "tag --force _ribbon origin/master";
       catchup = "log --patch --reverse --topo-order _ribbon..origin/master";
 
-      # # STASH
-      # sl  = "!git stash list | awk -F':' '{print $1}' | fzf --preview 'git -c color.ui=always stash show -p {}'";
-      # sp  = "!git stash pop `git stash list | awk -F':' '{print $1}' | fzf --preview 'git -c color.ui=always stash show -p {}'`";
-      # sd  = "!git stash drop `git stash list | awk -F':' '{print $1}' | fzf --preview 'git -c color.ui=always stash show -p {}'`";
+# # STASH
+# sl  = "!git stash list | awk -F':' '{print $1}' | fzf --preview 'git -c color.ui=always stash show -p {}'";
+# sp  = "!git stash pop `git stash list | awk -F':' '{print $1}' | fzf --preview 'git -c color.ui=always stash show -p {}'`";
+# sd  = "!git stash drop `git stash list | awk -F':' '{print $1}' | fzf --preview 'git -c color.ui=always stash show -p {}'`";
 
-      # # TOP COMMITTERS
-      # topcommiters = !"git log | grep Author | grep -v Authored | sort | uniq -c | sort -n -r";
+# # TOP COMMITTERS
+# topcommiters = !"git log | grep Author | grep -v Authored | sort | uniq -c | sort -n -r";
 
       identity = "! git config user.name \"$(git config user.$1.name)\"; git config user.email \"$(git config user.$1.email)\"; git config user.signingkey \"$(git config user.$1.signingkey)\"; :";
 

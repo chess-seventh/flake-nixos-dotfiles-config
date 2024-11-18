4dee553bdabaca1b4e7f37d9112dc788c85ea8de
Chess Seventh <chess7th@pm.me>
2024-11-18 17:17:13 +0100

    feat(networking): add hosts and full ip addresses

--- hosts/upsetter/laptop/networking.nix
+++ hosts/upsetter/laptop/networking.nix
@@ -2,6 +2,7 @@
 
 {
   networking = {
+    firewall.checkReversePath = "loose";
 
     useDHCP = lib.mkDefault true;
     # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
@@ -12,10 +13,16 @@
     networkmanager.enable = true;
 
     hosts = {
-      "100.78.206.44" = [ "nixos-01" ];
-      "100.93.19.41" = [ "nixos-02" ];
-      "100.108.2.81" = [ "nixos-03" ];
-      "100.122.160.144" = [ "bullwackies" ];
+      "nixos-01"        = [ "100.78.206.44"   "nixos-01.caracara-palermo.ts.net" ];
+      "nixos-02"        = [ "100.93.19.41"    "nixos-02.caracara-palermo.ts.net" ];
+      "nixos-03"        = [ "100.108.2.81"    "nixos-03.caracara-palermo.ts.net" ];
+      "nixos-blackark"  = [ "100.121.147.69"  "nixos-blackark.caracara-palermo.ts.net" ];
+      "bullwackies"     = [ "100.122.160.144" "bullwackies.caracara-palermo.ts.net" ];
+      "100.78.206.44"   = [ "nixos-01"        "nixos-01.caracara-palermo.ts.net" ];
+      "100.93.19.41"    = [ "nixos-02"        "nixos-02.caracara-palermo.ts.net" ];
+      "100.108.2.81"    = [ "nixos-03"        "nixos-03.caracara-palermo.ts.net" ];
+      "100.121.147.69"  = [ "nixos-blackark"  "nixos-blackark.caracara-palermo.ts.net" ];
+      "100.122.160.144" = [ "bullwackies"     "bullwackies.caracara-palermo.ts.net" ];
     };
 
     stevenblack = {

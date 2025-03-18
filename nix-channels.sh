b66fe9767338154e871b644262471e7a38ce46ed
Chess Seventh <chess7th@pm.me>
2025-03-18 11:00:39 +0100

    chore: script for updating the nixos-channels

--- nix-channels.sh
+++ nix-channels.sh
@@ -3,3 +3,7 @@
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
 sudo nix-channel --add https://nixos.org/channels/nixos-24.11 nixos
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
+nix-channel --add https://nixos.org/channels/nixos-24.11 nixos
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
